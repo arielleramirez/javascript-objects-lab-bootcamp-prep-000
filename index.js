eed160d09c5b1c89115b953c5234aeeb267dbc1f
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-26 01:10:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  delete object.key
-  return object
+  delete updatedObj.key
+  
+  updatedObj
+  
+  object
 }
