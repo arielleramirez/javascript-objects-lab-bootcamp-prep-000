ae2957e89ee32d9a87dde7f58a8eb945af95112e
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-26 00:48:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var recipes = {};
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  prop: 1, prop2: 2
+  return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  recipes = {prop: 1, prop2: 2};
+  return recipes
+}
