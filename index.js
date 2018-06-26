be05cc953c29a885cb8e250ed2575191bb5de612
Arielle N Ramirez <ariellenramirez@gmail.com>
2018-06-26 01:04:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes = {prop: 1, prop2: 2};
-  return recipes
+  return 
 }
